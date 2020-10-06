a59bee1dc1d99031a2289ce744bc75dc6964c97d
ironoa <alessio.onori@gmail.com>
2020-10-06 14:55:45 +0200

    improved judgement recognition

--- src/utils.ts
+++ src/utils.ts
@@ -104,9 +104,19 @@ export const isIdentitySetEvent = (event: Event): boolean => {
 export const isJudgementsFieldCompliant = (judgements: Vec<RegistrationJudgement>, registrarIndex: number): boolean =>{
   let isCompliant = false
   for (const judgement of judgements) {
-    if(judgement[0].toNumber() == registrarIndex) isCompliant = true
+    if(_isOurRegistrarTargetted(judgement,registrarIndex) && !_isAlreadyJudged(judgement)) isCompliant = true // add a further check here, it has not to be 
   }
   return isCompliant
+}
+
+const _isAlreadyJudged = (judgement: RegistrationJudgement) => {
+  if(!judgement[1]) return false
+  return judgement[1].isErroneous || judgement[1].isReasonable || judgement[1].isKnownGood || judgement[1].isLowQuality || judgement[1].isOutOfDate
+}
+
+const _isOurRegistrarTargetted = (judgement: RegistrationJudgement, registrarIndex: number) : boolean => {
+  if(!judgement[0]) return false
+  return judgement[0].toNumber() == registrarIndex
 }
 
 export const extractJudgementInfoFromEvent = (event: Event): JudgementRequest =>{
